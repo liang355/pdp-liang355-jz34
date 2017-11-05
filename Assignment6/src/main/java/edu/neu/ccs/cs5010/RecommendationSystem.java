5342edca70d40993de23eac57ada19baa46a9c8e
Jiawei Zhang <derek.zjw@gmail.com>
2017-11-04 17:50:42 -0700

    todo

--- Assignment6/src/main/java/edu/neu/ccs/cs5010/RecommendationSystem.java
+++ Assignment6/src/main/java/edu/neu/ccs/cs5010/RecommendationSystem.java
@@ -10,5 +10,8 @@ public class RecommendationSystem {
     InputParser inputParser = new InputParser(args);
     inputParser.checkArgs();
     //System.out.println(inputParser.getNumberOfRecommendations());  //for debug
+
+    //TODO：一个user 的 pq 按照被推荐次数倒序排
+    //TODO: 每次给一个用户推荐完，把被推荐人逐个加入pq，然后打印前十个
   }
 }
