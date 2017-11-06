67ea417b1d98ea568c35bf1b548d51c216c3852f
Jiawei Zhang <derek.zjw@gmail.com>
2017-11-05 17:07:39 -0800

    java doc update
    Recommendation and User java doc complete

--- Assignment6/src/main/java/edu/neu/ccs/cs5010/User.java
+++ Assignment6/src/main/java/edu/neu/ccs/cs5010/User.java
@@ -4,60 +4,99 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * User class represents the Twitter user and it's also the node of the social network graph.
+ * It has the date created, gender, age, city, be recommended times, follower list as the
+ * inward edge and following list as the outward edge in the social network graph.
+ */
 public class User {
-    //private int nodeId;
-    private String dateCreated;
-    private String gender;
-    private int age;
-    private String city;
-    private Set<Integer> followerList;
-    private Set<Integer> followingList;
-    private int recommendedTimes;
+  private String dateCreated;
+  private String gender;
+  private int age;
+  private String city;
+  private Set<Integer> followerList = new HashSet<>();
+  private Set<Integer> followingList = new HashSet<>();
+  private int recommendedTimes = 0;
 
-    public User(int nodeID, String dateCreated, String gender, int age, String city) {
-        //this.nodeId = nodeID;
-        this.dateCreated = dateCreated;
-        this.gender = gender;
-        this.age = age;
-        this.city = city;
-        followerList = new HashSet<>();
-        followingList = new HashSet<>();
-        this.recommendedTimes = 0;
+  /**
+   * Constructor with date created, gender, age, and city of the user.
+   * @param dateCreated of the account
+   * @param gender of the user
+   * @param age of the user
+   * @param city where the user is located
+   */
+  public User(String dateCreated, String gender, int age, String city) {
+    this.dateCreated = dateCreated;
+    this.gender = gender;
+    this.age = age;
+    this.city = city;
+  }
+
+  /**
+   * Add the otherUserId to the follower list of this user.
+   * @param otherUserId to be added to the follower list.
+   */
+  public void addFollower(int otherUserId) {
+    if (otherUserId != 0) { //handle the 0 ????????TODO:check
+      this.followerList.add(otherUserId);
     }
 
-    public void addFollower(int otherUserId) {
-        this.followerList.add(otherUserId);
-        //otherUser.followingList.add(this.nodeId);
-    }
+  }
 
-    public void addFollowing(int otherUserId) {
-        this.followingList.add(otherUserId);
-        //otherUser.followerList.add(this.nodeId);
-    }
+  /**
+   * Add the otherUserId to the following list of this user.
+   * @param otherUserId to be added to the following list.
+   */
+  public void addFollowing(int otherUserId) {
+    this.followingList.add(otherUserId);
+  }
 
-    public String getDateCreated() {
-        return dateCreated;
-    }
+  /**
+   * Returns the date created of the user's twitter account.
+   * @return the date created of the user's twitter account.
+   */
+  public String getDateCreated() {
+    return dateCreated;
+  }
 
-    public Set<Integer> getFollowerList() {
+  /**
+   * Returns the follower list of this user.
+   * @return the follower list of this user.
+   */
+  public Set<Integer> getFollowerList() {
         return followerList;
     }
 
-    public Set<Integer> getFollowingList() {
+  /**
+   * Returns the following list of this user.
+   * @return the following list of this user.
+   */
+  public Set<Integer> getFollowingList() {
         return followingList;
     }
 
-    public int getRecommendedTimes() {
+  /**
+   * Returns the times of being recommended of this user.
+   * @return the times of being recommended of this user.
+   */
+  public int getRecommendedTimes() {
         return recommendedTimes;
     }
 
-    public void incrementRecommendedTimes() {
+  /**
+   * Increment the beRecommendedTimes by one.
+   */
+  public void incrementRecommendedTimes() {
         this.recommendedTimes++;
     }
 
-    @Override
-    public String toString() {
-        return String.join(",", dateCreated, gender, String.valueOf(age), city,
-                Arrays.toString(followerList.toArray()), Arrays.toString(followerList.toArray()));
-    }
+  /**
+   * Represent the user in string.
+   * @return string representation of the user.
+   */
+  @Override
+  public String toString() {
+    return String.join(",", dateCreated, gender, String.valueOf(age), city,
+        Arrays.toString(followerList.toArray()), Arrays.toString(followerList.toArray()));
+  }
 }
