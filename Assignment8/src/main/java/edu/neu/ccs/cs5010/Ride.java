d13e6f2e688b696148239f24943f6fc772d78479
Ben Liang <liang355@umn.edu>
2017-11-26 02:23:02 -0800

    skier.dat random access

---
+++ Assignment8/src/main/java/edu/neu/ccs/cs5010/Ride.java
@@ -0,0 +1,32 @@
+package edu.neu.ccs.cs5010;
+
+public class Ride {
+    private String time;
+    private String liftId;
+
+    public Ride(String time, String liftId) {
+        this.time = time;
+        this.liftId = liftId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLiftId() {
+        return liftId;
+    }
+
+    public void setLiftId(String liftId) {
+        this.liftId = liftId;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + time + "," + liftId + "]";
+    }
+}
