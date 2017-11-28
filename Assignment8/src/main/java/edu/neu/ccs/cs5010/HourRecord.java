99c6a3788edf44b1b944fc5bc3361110a334ed5d
Jiawei Zhang <derek.zjw@gmail.com>
2017-11-27 16:42:04 -0800

    update

--- Assignment8/src/main/java/edu/neu/ccs/cs5010/HourRecord.java
+++ Assignment8/src/main/java/edu/neu/ccs/cs5010/HourRecord.java
@@ -2,18 +2,19 @@ package edu.neu.ccs.cs5010;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class HourRecord extends Hour {
-  public static final int SIZE = 2 * 4; ////????????
+  public static final int SIZE = 11 * Integer.BYTES; ////????????
 
   public HourRecord() {
     this(0,null);
   }
 
-  public HourRecord(int hourNum, Map<Integer, List<Integer>> hourRides) {
-    super(hourNum, hourRides);
+  public HourRecord(int hourNum, List<Integer> liftIdList) {
+    super(hourNum, liftIdList);
   }
 
   public void readFromFile(RandomAccessFile file)
@@ -23,6 +24,24 @@ public class HourRecord extends Hour {
       throw new IllegalArgumentException("given hourNum does not exist in records!!");
     }
     setHourNum(hourNum);
+    setLiftIdList(readList(file));
+  }
+
+  public void writeToFile(RandomAccessFile file)
+      throws IOException {
+    file.writeInt(getHourNum());
+    for (int liftId : getLiftIdList()) {
+      file.writeInt(liftId);
+    }
+  }
+
+  private List readList(RandomAccessFile file)
+      throws IOException {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 1; i <= 10; i++) {
+      res.add(file.readInt());
+    }
+    return res;
   }
 
 
