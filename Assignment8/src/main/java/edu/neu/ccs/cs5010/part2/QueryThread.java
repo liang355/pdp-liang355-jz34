7f8c0e1c2e35327cc86e7a4b97aedc2a8de11beb
Ben Liang <liang355@umn.edu>
2017-11-27 17:16:48 -0800

    skier query

--- Assignment8/src/main/java/edu/neu/ccs/cs5010/part2/QueryThread.java
+++ Assignment8/src/main/java/edu/neu/ccs/cs5010/part2/QueryThread.java
@@ -6,26 +6,30 @@ import edu.neu.ccs.cs5010.SkierRecord;
 
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 
 public class QueryThread extends Thread {
-    private Queue<String> queryQueue = new LinkedList<>();
+    private List<String[]> rows = new LinkedList<>();
     private StringBuilder skierSB = new StringBuilder("");
     private ReadWriteCsv readWriteCsv = new ReadWriteCsv();
     private SkierFileEditor skierFE;
     private int threadId;
 
-    public QueryThread(Queue<String> queryQueue, int threadId) throws IOException{
-        this.queryQueue = queryQueue;
+    public QueryThread(List<String[]> rows, int threadId) {
+        this.rows = rows;
         this.threadId = threadId;
-        skierFE = new SkierFileEditor("skier.dat");
+        try {
+            skierFE = new SkierFileEditor("skier.dat");
+        } catch (IOException ioe) {
+            System.out.println("*** OOPS! Something is wrong when reading file!");
+        }
     }
 
     @Override
     public void run() {
         String outputFileName = "results_" + threadId + ".txt";
-        while(!queryQueue.isEmpty()) {
-            String[] currPair = queryQueue.poll().split(",");
+        for(int i = 0; i < rows.size(); i++) {
+            String[] currPair = rows.get(i);
             String queryType = currPair[0];
             String queryParam = currPair[1];
             try {
