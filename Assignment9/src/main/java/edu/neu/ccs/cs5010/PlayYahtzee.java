f174e79586a74257c6ccfbf7290d2752580b7411
Jiawei Zhang <derek.zjw@gmail.com>
2017-12-01 16:22:25 -0500

    connection finish

--- Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
+++ Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
@@ -1,4 +1,16 @@
 package edu.neu.ccs.cs5010;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class PlayYahtzee {
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.err.println("Usage: java PlayYah");
+    }
+
+  }
 }
