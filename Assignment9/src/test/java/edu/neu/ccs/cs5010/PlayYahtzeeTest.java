21306a0d20ec682ecb240a599a1406a30e02d9bd
Jiawei Zhang <derek.zjw@gmail.com>
2017-12-04 14:25:43 -0800

    update

---
+++ Assignment9/src/test/java/edu/neu/ccs/cs5010/PlayYahtzeeTest.java
@@ -0,0 +1,31 @@
+package edu.neu.ccs.cs5010;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.net.Socket;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class PlayYahtzeeTest {
+  //PlayYahtzee test = new PlayYahtzee();
+  String[] args = new String[]{"localhost","1200"};
+
+  @Test
+  public void main() throws Exception {
+    //byte[] emptyPayload = new byte[1001];
+
+//    final Socket socket = mock(Socket.class);
+//    final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//    when(socket.getOutputStream()).thenReturn(byteArrayOutputStream);
+
+    PlayYahtzee test = new PlayYahtzee();
+    test.main(args);
+
+//    Mockito.when(mockGameService.getAllChoices()).thenReturn(expected); //mock the service here with user defined expected result
+//    assertEquals(expected, gameController.getAllChoices());
+//    Mockito.verify(mockGameService, Mockito.times(1)).getAllChoices(); //to verify wanted number of invocations of a function
+  }
+
+}
--- Assignment9/src/test/java/edu/neu/ccs/cs5010/ServerMessageParserTest.java
+++ Assignment9/src/test/java/edu/neu/ccs/cs5010/ServerMessageParserTest.java
@@ -8,6 +8,9 @@ import java.util.Map;
 
 import static org.junit.Assert.*;
 
+/**
+ * Junit test for ServerMessageParser.
+ */
 public class ServerMessageParserTest {
   ServerMessageParser test = new ServerMessageParser();
 
